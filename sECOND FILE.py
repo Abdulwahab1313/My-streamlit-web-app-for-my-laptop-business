78990fccde4f7b3abc36da3cf4b639939e52b2c9
Abdulwahab1313 <ahmedabdulwahab1212@gmail.com>
2025-12-04 20:26:41 +0100

    Refactor and clean up sECOND FILE.py

--- sECOND FILE.py
+++ sECOND FILE.py
@@ -1,10 +1,10 @@
 import streamlit as st
 st.title("Welcome to Aws Gadget")
 tab1,tab2,tab3,tabs4,tab5,tab6= st.tabs(["Hp","Dell","Lenovo","Samsung","acer","Random mini pc" ])
-link = st.markdown("[clikck link to chat me up](https://wa.me/2348066799167)")
+link = "[clikck link to chat me up](https://wa.me/2348066799167)"
 with tab1:
     img = st.image("IMG-20251203-WA0009.jpg", width=200,caption="hp probook touchscreen.keyboardlight,512ssd,8gbram,N400000")
-    st.write(link)
+    st.markdown(link)
     with st.expander("click for more photos and details"):
         lists=["Hp11.jpg","Hp12.jpg", "Hp13.jpg"]
         sl1 = st.slider("increase image1 size", 100, 300)
@@ -12,7 +12,7 @@ with tab1:
         st.write(sl1)
 
     img1 = st.image("Hp14.jpg", width=200,caption="hp Elitebook g420 touchscreen.keyboardlight,512ssd,8gbram,N330000")
-    st.write(link)
+    st.markdown(link)
     with st.expander("click for more photos"):
         lists = ["Hp15.jpg", "Hp16.jpg","Hp17.jpg","Hp18.jpg"]
         sl2= st.slider("increase image2 size",100,300)
@@ -22,7 +22,7 @@ with tab1:
 
 with tab5:
     img2 = st.image("acer11.jpg", width=200,caption="hp probook touchscreen.keyboardlight,512ssd,8gbram,N40000")
-    st.markdown("[clikck link to chat me up](wa.me/+234 806 679 9167)")
+    st.markdown(link)
     with st.expander("click for more photos and details"):
         lists=["acer12.jpg","acer13.jpg","acer14"]
         sl3 = st.slider("increase image3 size", 100, 300)
@@ -31,7 +31,7 @@ with tab5:
 
 with tab3:
     img2 = st.image("acer11.jpg", width=200,caption="hp probook touchscreen.keyboardlight,512ssd,8gbram,N40000")
-    st.write(link)
+    st.markdown(link)
     with st.expander("click for more photos and details"):
         lists=["acer12.jpg","acer13.jpg","acer14"]
         sl3 = st.slider("increase image3 size", 100, 300)
@@ -40,7 +40,7 @@ with tab3:
 
 with tab5:
     img2 = st.image("acer11.jpg", width=200,caption="hp probook touchscreen.keyboardlight,512ssd,8gbram,N40000")
-    st.write(link)
+    st.markdown(link)
     with st.expander("click for more photos and details"):
         lists=["acer12.jpg","acer13.jpg","acer14"]
         sl3 = st.slider("increase image3 size", 100, 300)
@@ -49,7 +49,7 @@ with tab5:
 
 with tab5:
     img2 = st.image("acer11.jpg", width=200,caption="hp probook touchscreen.keyboardlight,512ssd,8gbram,N40000")
-    st.write(link)
+    st.markdown(link)
     with st.expander("click for more photos and details"):
         lists=["acer12.jpg","acer13.jpg","acer14"]
         sl3 = st.slider("increase image3 size", 100, 300)
@@ -58,12 +58,13 @@ with tab5:
 
 with tab5:
     img2 = st.image("acer11.jpg", width=200,caption="hp probook touchscreen.keyboardlight,512ssd,8gbram,N40000")
-    st.write(link)
+    st.markdown(link)
     with st.expander("click for more photos and details"):
         lists=["acer12.jpg","acer13.jpg","acer14"]
         sl3 = st.slider("increase image3 size", 100, 300)
         st.image(lists, width=sl3)
         st.write(sl3)
+
 
 
 
