1c17d1a036ffe44d1ce9c4dd2c86e1d4c43a5ba3
Abdulwahab1313 <ahmedabdulwahab1212@gmail.com>
2025-12-06 05:31:04 +0100

    Update sECOND FILE.py

--- sECOND FILE.py
+++ sECOND FILE.py
@@ -11,29 +11,29 @@ with tab1:
         st.image(lists, width=sl1)
         st.write(sl1)
 
-    img1 = st.image("Hp15.jpg", width=200,caption="hp Elitebook g420 touchscreen.keyboardlight,512ssd,8gbram,N330000")
+    img1 = st.image("https://github.com/Abdulwahab1313/My-streamlit-web-app-for-my-laptop-business/raw/main/Hp15.jpg", width=200,caption="hp Elitebook g420 touchscreen.keyboardlight,512ssd,8gbram,N330000")
     st.markdown(link)
     with st.expander("click for more photos"):
-        lists = ["Hp16.jpg","Hp17.jpg","Hp18.jpg"]
+        lists = ["https://github.com/Abdulwahab1313/My-streamlit-web-app-for-my-laptop-business/raw/main/Hp16.jpg","https://github.com/Abdulwahab1313/My-streamlit-web-app-for-my-laptop-business/raw/main/Hp17.jpg","https://github.com/Abdulwahab1313/My-streamlit-web-app-for-my-laptop-business/raw/main/Hp18.jpg"]
         sl2= st.slider("increase image2 size",100,300)
         st.image(lists, width=sl2)
         st.write(sl2)
 
 
 with tab5:
-    img2 = st.image("acer11.jpg", width=200,caption="hp probook touchscreen.keyboardlight,512ssd,8gbram,N40000")
+    img2 = st.image("https://github.com/Abdulwahab1313/My-streamlit-web-app-for-my-laptop-business/raw/main/acer11.jpg", width=200,caption="acer,500GBhdd,8gbram,N130000")
     st.markdown(link)
     with st.expander("click for more photos and details"):
-        lists=["acer12.jpg","acer13.jpg","acer14.jpg"]
+        lists=["https://github.com/Abdulwahab1313/My-streamlit-web-app-for-my-laptop-business/raw/main/acer12.jpg","https://github.com/Abdulwahab1313/My-streamlit-web-app-for-my-laptop-business/raw/main/acer13.jpg","https://github.com/Abdulwahab1313/My-streamlit-web-app-for-my-laptop-business/raw/main/acer14.jpg"]
         sl3 = st.slider("increase image3 size", 100, 300)
         st.image(lists, width=sl3)
         st.write(sl3)
 
 with tab3:
-    img2 = st.image("acer11.jpg", width=200,caption="hp probook touchscreen.keyboardlight,512ssd,8gbram,N40000")
+    img2 = st.image("https://github.com/Abdulwahab1313/My-streamlit-web-app-for-my-laptop-business/raw/main/Dell11.jpg", width=200,caption="hp probook touchscreen.keyboardlight,512ssd,8gbram,N180000")
     st.markdown(link)
     with st.expander("click for more photos and details"):
-        lists=["acer12.jpg","acer13.jpg","acer14.jpg"]
+        lists=["https://github.com/Abdulwahab1313/My-streamlit-web-app-for-my-laptop-business/raw/main/Dell12.jpg","https://github.com/Abdulwahab1313/My-streamlit-web-app-for-my-laptop-business/raw/main/Dell13.jpg"]
         sl3 = st.slider("increase image4 size", 100, 300)
         st.image(lists, width=sl3)
         st.write(sl3)
@@ -57,10 +57,10 @@ with tab5:
         st.write(sl3)
 
 with tab6:
-    img2 = st.image("acer11.jpg", width=200,caption="hp probook touchscreen.keyboardlight,512ssd,8gbram,N40000")
+    img2 = st.image("https://github.com/Abdulwahab1313/My-streamlit-web-app-for-my-laptop-business/raw/main/Random mini lenovo pc 5.jpg", width=200,caption="hp probook touchscreen.keyboardlight,512ssd,8gbram,N40000")
     st.markdown(link)
     with st.expander("click for more photos and details"):
-        lists=["Random mini lenovo pc1.jpg","Random mini lenovo pc 2.jpg","Random mini lenovo pc 3.jpg","Random mini lenovo pc 4.jpg","Random mini lenovo pc 5.jpg"]
+        lists=["https://github.com/Abdulwahab1313/My-streamlit-web-app-for-my-laptop-business/raw/main/Random mini lenovo pc1.jpg","https://github.com/Abdulwahab1313/My-streamlit-web-app-for-my-laptop-business/raw/main/Random mini lenovo pc 2.jpg","https://github.com/Abdulwahab1313/My-streamlit-web-app-for-my-laptop-business/raw/main/Random mini lenovo pc 3.jpg","https://github.com/Abdulwahab1313/My-streamlit-web-app-for-my-laptop-business/raw/main/Random mini lenovo pc 4.jpg","https://github.com/Abdulwahab1313/My-streamlit-web-app-for-my-laptop-business/raw/main/Random mini lenovo pc 5.jpg"]
         sl3 = st.slider("increase image7 size", 100, 300)
         st.image(lists, width=sl3)
         st.write(sl3)
